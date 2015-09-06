ed05d6157b0bcfd5c4750662e72567bde2d07b9c
Yoann Moinet <yoann.moinet@gmail.com>
2015-09-06 02:26:37 -0400

    docs: add demo and debug

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,6 +2,92 @@ var joystick = nipplejs.create({
 	zone: document.getElementById('zone_joystick')
 });
 
-joystick.on('start', function (data) {
+// Get debug elements and map them
+var elDebug = document.getElementById('debug');
+var elDump = elDebug.querySelector('.dump');
+var els = {
+	position: {
+		x: elDebug.querySelector('.position .x .data'),
+		y: elDebug.querySelector('.position .y .data')
+	},
+	force: elDebug.querySelector('.force .data'),
+	distance: elDebug.querySelector('.distance .data'),
+	angle: {
+		radian: elDebug.querySelector('.angle .radian .data'),
+		degree: elDebug.querySelector('.angle .degree .data')
+	},
+	direction: {
+		x: elDebug.querySelector('.direction .x .data'),
+		y: elDebug.querySelector('.direction .y .data'),
+		angle: elDebug.querySelector('.direction .angle .data')
+	}
 
+};
+
+joystick.on('start', function (evt, data) {
+	dump(evt.type);
+	debug(data);
 });
+joystick.on('move', function (evt, data) {
+	dump(evt.type);
+	debug(data);
+});
+joystick.on('dir', function (evt, data) {
+	dump(evt.type);
+});
+joystick.on('plain', function (evt, data) {
+	dump(evt.type);
+});
+joystick.on('dir:top', function (evt, data) {
+	dump(evt.type);
+});
+joystick.on('plain:top', function (evt, data) {
+	dump(evt.type);
+});
+joystick.on('dir:left', function (evt, data) {
+	dump(evt.type);
+});
+joystick.on('plain:left', function (evt, data) {
+	dump(evt.type);
+});
+joystick.on('dir:bottom', function (evt, data) {
+	dump(evt.type);
+});
+joystick.on('plain:bottom', function (evt, data) {
+	dump(evt.type);
+});
+joystick.on('dir:right', function (evt, data) {
+	dump(evt.type);
+});
+joystick.on('plain:right', function (evt, data) {
+	dump(evt.type);
+});
+joystick.on('end', function (evt, data) {
+	dump(evt.type);
+	debug(data);
+});
+
+// Print data into elements
+function debug (obj) {
+	function parseObj (sub, el) {
+		for (var i in sub) {
+			if (typeof sub[i] === 'object') {
+				parseObj(sub[i], el[i]);
+			} else {
+				el[i].innerHTML = sub[i];
+			}
+		}
+	}
+	parseObj(obj, els);
+}
+var nbEvents = 0;
+// Dump data
+function dump (evt) {
+	if (elDump.children.length > 5) {
+		elDump.removeChild(elDump.firstChild);
+	}
+	var newEvent = document.createElement('div');
+	newEvent.innerHTML = '#' + nbEvents + ' : <span class="data">' + evt + '</span>';
+	elDump.appendChild(newEvent);
+	nbEvents += 1;
+}
