f4cd3045943c3f6c4caea454ae2544753e6efc59
Yoann Moinet <yoann.moinet@autodesk.com>
2015-09-09 10:58:16 -0400

    fix: let https pass again

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,7 @@
 var elDemo = document.getElementsByTagName('h2')[1].nextElementSibling;
-elDemo.innerHTML = 'See the Pen <a href="http://codepen.io/YoannM/pen/gapmMG/">nippleJS</a>'+
-	'by Yoann Moinet (<a href="http://codepen.io/YoannM">@YoannM</a>) on'+
-	'<a href="http://codepen.io">CodePen</a>.';
+elDemo.innerHTML = 'See the Pen <a href="//codepen.io/YoannM/pen/gapmMG/">nippleJS</a>'+
+	'by Yoann Moinet (<a href="//codepen.io/YoannM">@YoannM</a>) on'+
+	'<a href="//codepen.io">CodePen</a>.';
 elDemo.className = 'codepen';
 elDemo.setAttribute('data-user', 'YoannM');
 elDemo.setAttribute('data-default-tab', 'result');
@@ -10,6 +10,6 @@ elDemo.setAttribute('data-theme-id', '1775');
 elDemo.setAttribute('data-height', '735');
 
 var codepenScript = document.createElement('script');
-codepenScript.src = 'http://assets.codepen.io/assets/embed/ei.js';
+codepenScript.src = '//assets.codepen.io/assets/embed/ei.js';
 codepenScript.async = true;
 document.body.appendChild(codepenScript);
