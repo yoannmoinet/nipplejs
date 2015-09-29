87fc187491eb95010e1637ce228e236c4d7e2246
Yoann Moinet <yoann.moinet@gmail.com>
2015-09-28 23:41:49 -0400

    fix(demo): update demo's height

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,13 +1,13 @@
 var elDemo = document.getElementsByTagName('h2')[1].nextElementSibling;
-elDemo.innerHTML = 'See the Pen <a href="//codepen.io/YoannM/pen/gapmMG/">nippleJS</a>'+
-	'by Yoann Moinet (<a href="//codepen.io/YoannM">@YoannM</a>) on'+
+elDemo.innerHTML = 'See the Pen <a href="//codepen.io/YoannM/pen/gapmMG/">nippleJS</a>' +
+	'by Yoann Moinet (<a href="//codepen.io/YoannM">@YoannM</a>) on' +
 	'<a href="//codepen.io">CodePen</a>.';
 elDemo.className = 'codepen';
 elDemo.setAttribute('data-user', 'YoannM');
 elDemo.setAttribute('data-default-tab', 'result');
 elDemo.setAttribute('data-slug-hash', 'gapmMG');
 elDemo.setAttribute('data-theme-id', '1775');
-elDemo.setAttribute('data-height', '735');
+elDemo.setAttribute('data-height', '1000');
 
 var codepenScript = document.createElement('script');
 codepenScript.src = '//assets.codepen.io/assets/embed/ei.js';
